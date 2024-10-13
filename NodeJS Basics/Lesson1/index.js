b4de6b0b9852ace1c803769245042afcb762f729
ProvaPaul <paul2007037@stud.kuet.ac.bd>
2024-10-13 09:35:04 +0600

    Local module - exports, require

---
+++ NodeJS Basics/Lesson1/index.js
@@ -0,0 +1,13 @@
+const s1= require('./student')
+// console.log(s1.getAge())
+// console.log(s1.getName())
+// console.log(s1.cgpa)
+
+// for finding only selective function
+// const {getName, getSection}=require('./student')
+// console.log(getName())
+
+console.log(s1.getSection())
+
+
+
---
+++ NodeJS Basics/Lesson1/student.js
@@ -0,0 +1,24 @@
+const getName=()=>{
+    return "Anisul islam"
+}
+
+const getAge=()=>{
+    return "23"
+}
+const cgpa=3.99;
+
+// exports.getAge=getAge;
+// exports.getName=getName;
+// exports.cgpa=cgpa;
+
+// for exporting in one line
+
+// module.exports={
+//     getAge,getName,cgpa
+// }
+
+// for directly exporting a function
+
+exports.getSection=()=>{
+    return "sectionA";
+}
