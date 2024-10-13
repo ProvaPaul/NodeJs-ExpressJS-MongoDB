0862baf87ae7db0fa65f471d5ac120a685bf2551
ProvaPaul <paul2007037@stud.kuet.ac.bd>
2024-10-13 15:38:10 +0600

    Module | external modules | npm

---
+++ NodeJS Basics/Lesson6/index.js
@@ -0,0 +1,14 @@
+// for learning external module
+// 1.Now install a package for fruit-name
+//package.json file will be automatically added after running the command npm init into the terminal
+// package install er pore require korte hobe
+
+// const randomFruitsName=require("random-fruits-name");
+// // console.log(randomFruitsName())
+
+// console.log(randomFruitsName("en"))
+
+
+// 2.Now install a package for movie-name
+var moviesNames = require('movies-names');
+console.log(moviesNames.random()) 
