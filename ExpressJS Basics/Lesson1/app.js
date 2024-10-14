704686599593ea32caeef0cf257efc06c679ac4c
ProvaPaul <paul2007037@stud.kuet.ac.bd>
2024-10-14 08:52:47 +0600

    http methods and postman

--- ExpressJS Basics/Lesson1/app.js
+++ ExpressJS Basics/Lesson1/app.js
@@ -1,4 +1,29 @@
 const express=require("express");
 const app=express();
 
+// route
+// 1.get(browser theke req dile)
+app.get("/",(req,res)=>{
+    res.send("I am a get request at home route");
+})
+app.get("/about",(req,res)=>{
+    res.send("I am a get request at about route");
+})
+// 2.post method(serever will receive and fetch and give result)->download postman software
+
+app.post("/",(req,res)=>{
+    res.send("I am a post request at home route");
+})
+// 3.put
+app.put("/",(req,res)=>{
+    res.send("I am a put request at home route");
+})
+// 3.patch
+app.patch("/",(req,res)=>{
+    res.send("I am a patch request at home route");
+})
+// 5.delete
+app.delete("/",(req,res)=>{
+    res.send("I am a delete request at home route");
+})
 module.exports=app;
