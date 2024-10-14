52b863483cf6ce4a0c7b3c194ca4154202b201cb
ProvaPaul <paul2007037@stud.kuet.ac.bd>
2024-10-14 21:02:33 +0600

    express static middleware

--- ExpressJS Basics/Lesson5/index.js
+++ ExpressJS Basics/Lesson5/index.js
@@ -2,33 +2,38 @@ const express=require('express');
 const app=express();
 const port=3000;
 
-const myMiddleWare=(req,res,next)=>{
-    console.log('I am a middleware');
-    req.currentTime=new Date(Date.now());
-    next();
-};
-app.use(myMiddleWare);
-app.get("/",myMiddleWare,(req,res)=>{ 
-    console.log('I am a route handler'+req.currentTime);                                      
-    res.send('Hello World');
-});
+// const myMiddleWare=(req,res,next)=>{
+//     console.log('I am a middleware');
+//     req.currentTime=new Date(Date.now());
+//     next();
+// };
+// // 1.application level middleware
+// app.use(myMiddleWare);
+// app.get("/",myMiddleWare,(req,res)=>{ 
+//     console.log('I am a route handler'+req.currentTime);                                      
+//     res.send('Hello World');
+// });
 
-app.get("/about",myMiddleWare,(req,res)=>{ 
-    console.log('I am about route handler'+req.currentTime);                                      
-    res.send('Hello World about page');
-});
+// app.get("/about",myMiddleWare,(req,res)=>{ 
+//     console.log('I am about route handler'+req.currentTime);                                      
+//     res.send('Hello World about page');
+// });
 
-// error handling middleware(not found type middleware)
-app.use((req,res,next)=>{
-    res.status(404).send('Not Found');
+// //2.error handling middleware(not found type middleware)
+// app.use((req,res,next)=>{
+//     res.status(404).send('Not Found');
+// });
+// // more error
+// app.use((err,req,res,next)=>{
+//     console.error(err.stack);   
+//     res.status(500).send('Something broke!');
+// }   
+// );
+// 3.static middleware
+app.use(express.static('public'));
+app.get("/",(req,res)=>{ 
+    res.sendfile(__dirname+'/index.html');
 });
-// more error
-app.use((err,req,res,next)=>{
-    console.error(err.stack);   
-    res.status(500).send('Something broke!');
-}   
-);
-
 app.listen(port,()=>{    
     console.log(`Server is running at http://localhost:${port}`);
 });
