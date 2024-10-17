78a8fd7e5e2a1fc59c20a46424cbcfa550da8476
ProvaPaul <paul2007037@stud.kuet.ac.bd>
2024-10-17 18:03:12 +0600

    save data in database from express server

--- MongoDB/MongoDB/index.js
+++ MongoDB/MongoDB/index.js
@@ -4,6 +4,10 @@ const { type } = require('os');
 
 const app=express();
 const port=3000;
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
 // found this from mongoose website
 // 1.without async await function
 //  mongoose.connect('mongodb://127.0.0.1:27017/testProductDB')
@@ -15,14 +19,20 @@ const port=3000;
 // id ,title,price,description,date
 // 1. create a schema
 const productSchema= new mongoose.Schema({
-    title:String,
-    // {
-    //     type:String,
-    //     required:true
-    //     // required true means it is mandatory to have a title
-    // },
-    price: Number,
-    description:String,
+    title:
+    {
+        type:String,
+        required:true
+        // required true means it is mandatory to have a title
+    },
+    price: {
+        type:Number,
+        required:true
+    },
+    description:{
+        type:String,
+        required:true
+    },
     createdAt:{
         type:Date,
         default:Date.now
@@ -31,7 +41,7 @@ const productSchema= new mongoose.Schema({
 // 2. create a model
 const Product=mongoose.model('Products',productSchema);
 
-// 3. create a document
+// connection create a document
 // database->collection(table)->document(row)
 
 const connectDB = async()=>{
@@ -48,6 +58,29 @@ const connectDB = async()=>{
 app.get('/',(req,res)=>{    
     res.send('Hello World');
 });
+// create a route
+app.post('/products',async(req,res)=>{    
+    try{
+        // get data from request body
+        // const title=req.body.title;
+        // const price=req.body.price;
+        // const description=req.body.description;
+        // create a model
+        const newProduct=new Product({
+            title:req.body.title,
+            price:req.body.price,
+            description:req.body.description
+        });
+        // for storing data in database
+        const productData=await newProduct.save();
+        res.status(201).send(productData);
+
+    }
+    catch(err){
+        res.status(400).send({ message:err.message});
+    }
+});
+
 app.listen(port,async()=>{
     console.log(`Server is running on port ${port}`);
     await connectDB()
